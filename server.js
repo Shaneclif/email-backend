e1187413586770c6f7a4924aa95344f43991e485
Shaneclif <shaneclifton17@gmail.com>
2025-06-06 23:20:56 +0200

    Updated database to wakatv.sqlite and server logging

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = require('./db');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(bodyParser.json());
